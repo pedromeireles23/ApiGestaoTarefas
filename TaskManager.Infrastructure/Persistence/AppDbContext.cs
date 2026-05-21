3f474e8f118959835a5b779ff68e43b56f073c6d
pedromeireles23 <pemeirels@gmail.com>
2026-05-21 01:59:22 -0300

    Inicializando a conexão com o banco de dados

---
+++ TaskManager.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Application.Entities;
+
+namespace TaskManager.Infrastructure.Persistence
+{
+  public class AppDbContext : DbContext
+  {
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+      
+    }
+    public DbSet<Tenant> Tenants {get;set;}
+    public DbSet<User> Users {get;set;}
+    public DbSet<Workspace> Workspaces {get;set;}
+    public DbSet<Project> Projects {get;set;}
+    public DbSet<TaskItem> TaskItems {get;set;}
+  }
+}
